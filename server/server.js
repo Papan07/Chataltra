204f6273e3bff0d1e02b61d3b4c1673b031f3e48
Papan Namasudra <papanns19@gmail.com>
2025-08-26 11:04:31 +0530

    server

--- server/server.js
+++ server/server.js
@@ -129,7 +129,7 @@ app.use((req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
   console.log(`Environment: ${process.env.NODE_ENV}`);
   console.log(`Socket.IO server is ready`);
